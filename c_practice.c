b38438ba9ff91c48cb8c0cae5476834338c2dec3
Ashish Raj <ashishraj45232@gmail.com>
2022-04-02 17:57:36 +0530

    Initial commit

---
+++ c_practice.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+// write a program to see if a number is divisible by 97 or not
+int main() {
+    int num;
+    printf("Enter the number \n");
+    scanf("%d", &num);
+    printf("Divisibility test returns: %d \n", num%97);
+// step by step evaluation of 3*x/y-z+k
+
+
+
+    int x = 2, y=3, z=3, k=1 ;
+    int result = 3 * x / y - z + k ;
+    printf("The value of result is %d", result);
+    return 0;
+}
