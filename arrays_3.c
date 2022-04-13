c580ed15c8765870fd16418ebc95be4875433ca0
arc-005 <102738579+arc-005@users.noreply.github.com>
2022-04-13 18:59:51 +0530

    Add files via upload

---
+++ arrays_3.c
@@ -0,0 +1,17 @@
+// create an array of 10 numbers. Verify using pointer arithmetic that (ptr + 2) points to the third element where ptr is a pointer printing to the first element of the
+#include<stdio.h>
+
+int main() {
+    int arr[10];
+    int *ptr = arr;
+    ptr = ptr +2;
+
+    if(ptr==&arr[2]){
+        printf("These point to the same location in memory\n");
+    }
+
+    else{
+        printf("These do not point to the same location in memory\n");
+    }
+    return 0;
+}
