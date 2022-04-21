59087de120459f7042b5ac1f70a6ef549df4f0ee
arc-005 <102738579+arc-005@users.noreply.github.com>
2022-04-21 18:09:51 +0530

    Add files via upload

---
+++ structures_02.c
@@ -0,0 +1,21 @@
+// create a two dimensional vector using structures.
+#include<stdio.h>
+struct vector
+{
+    int x;
+    int y;
+};
+
+int main() {
+    struct vector v1, v2;
+    v1.x = 34;
+    v1.y = 54;
+    printf("X dimension is %d and Y dimension is %d \n", v1.x, v1.y);
+
+    v2.x = 323;
+    v2.y = 534;
+    printf("X dimension is %d and Y dimension is %d \n", v2.x, v2.y);
+
+  
+    return 0;
+}
