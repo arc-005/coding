857626816151d6aa46cdfbb39c695663d80fafe7
arc-005 <102738579+arc-005@users.noreply.github.com>
2022-04-27 17:49:20 +0530

    Add files via upload

---
+++ main3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+int glo =9;
+glo =133;
+int a = 6;
+int b = 9;
+float pi = 3.1415;
+char c = 'a';
+bool s = true;
+cout<<glo;
+cout<<"\n The value of a is "<<a<<".\n The value of b is "<<b;
+cout<<"\nThe value of pi is: "<<pi;
+cout<<"\nThe value of character is: "<<c;
+cout<<"\n"<<s;
+
+
+return 0;
+
+}
