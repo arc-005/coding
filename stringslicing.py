9a17bf9604750f09bef73024391ab972f73379b2
arc-005 <102738579+arc-005@users.noreply.github.com>
2022-05-16 22:01:11 +0530

    Add files via upload

---
+++ stringslicing.py
@@ -0,0 +1,18 @@
+# string concatenation
+greeting = "Good Morning, "
+name = "Ashish"
+print(greeting + name)
+
+print(name[3])
+# name[3] = "Z"------> does not work
+
+print(name[1:4])
+# print(name[0:])------> it will also work
+# print(name[:4]) -----> it will also work
+print(name[-6])
+
+name = "EVERYTHINGWILLBEFINE."
+d = name[0::2]
+print(d)
+
+
